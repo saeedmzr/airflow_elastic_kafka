d3c5f717e0ecf496eafedac94710cc83490b6a48
saeedmouzarmi <saeedmouzarmi@gmail.com>
2023-06-14 17:13:10 +0330

    update kafka

--- dags/components/kafka.py
+++ dags/components/kafka.py
@@ -1,14 +1,15 @@
 from confluent_kafka import Producer
 import json
 
+
 class Kafka:
     def __init__(self, BOOTSTRAP, TOPIC, USERNAME, PASSWORD):
         self.producer = Producer({
             'bootstrap.servers': BOOTSTRAP,
-            # 'security.protocol': 'SASL_PLAINTEXT',
-            # 'sasl.mechanism': 'PLAIN',
-            # 'sasl.username': USERNAME,
-            # 'sasl.password': PASSWORD
+            'security.protocol': 'SASL_PLAINTEXT',
+            'sasl.mechanism': 'PLAIN',
+            'sasl.username': USERNAME,
+            'sasl.password': PASSWORD
         })
         self.kafka_topic = TOPIC
 
