3cad3d6d04dd0bc28971d7ba8b1635700cf9b3a5
Amirreza Akbari <amrakbaridev@gmail.com>
2023-10-02 17:26:12 +0330

    WIP

--- dags/expand.py
+++ dags/expand.py
@@ -28,7 +28,7 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 
-with DAG(dag_id="add_emotion_tag_with_expand", default_args=default_args, schedule_interval='*/1 * * * *',
+with DAG(dag_id="add_emotion_ner_sentiment_tag", default_args=default_args, schedule_interval='*/1 * * * *',
          catchup=False, max_active_tasks=20, max_active_runs=1) as dag:
     @task()
     def reterieve_data_from_elastic():
@@ -53,16 +53,6 @@ with DAG(dag_id="add_emotion_tag_with_expand", default_args=default_args, schedu
                         {
                             "exists": {
                                 "field": "ner"
-                            }
-                        },
-                        {
-                            "exists": {
-                                "field": "lf_subject"
-                            }
-                        },
-                        {
-                            "exists": {
-                                "field": "subject"
                             }
                         },
                         {
@@ -138,6 +128,7 @@ with DAG(dag_id="add_emotion_tag_with_expand", default_args=default_args, schedu
                 results.append(None)
         return results
 
+
     @task(max_active_tis_per_dag=3)
     def perform_emotion_v1(texts):
         emotion = Emotion('instagram')
@@ -211,7 +202,126 @@ with DAG(dag_id="add_emotion_tag_with_expand", default_args=default_args, schedu
                     },
                     "ner": {
                         "version": "1.0.0"
-                    },
+                    }
+                }
+            }
+            kafka_client.insert_data(tmp_data)
+            final_data.append(tmp_data)
+        kafka_client.producer.flush()
+        return final_data
+
+
+    @task()
+    def prepare_data(raw_data):
+        pool_array = []
+        tran_data = [item["_source"] for item in raw_data]
+        batch_size = int(setting.BATCH_SIZE)
+        while len(tran_data):
+            init_data = tran_data[:batch_size]
+            pool_array.append(init_data)
+            del tran_data[:batch_size]
+
+        return pool_array
+
+
+    raw_data = reterieve_data_from_elastic()
+    prepared_data = prepare_data(raw_data)
+    ner_tags = perform_ner.expand(batch=prepared_data)
+    emotion_v1_tags = perform_emotion_v1.expand(texts=prepared_data)
+    sentiment_tags = perform_sentiment.expand(batch=prepared_data)
+    final_data = print_output(
+        raw_data=raw_data,
+        ner_result=ner_tags,
+        sentimant_result=sentiment_tags,
+        emotion_v1_result=emotion_v1_tags,
+        subject_v1_result=subject_v1_tags,
+        subject_v2_result=subject_v2_tags,
+    )
+
+with DAG(dag_id="add_subject_tag", default_args=default_args, schedule_interval='*/1 * * * *',
+         catchup=False, max_active_tasks=20, max_active_runs=1) as dag:
+    @task()
+    def reterieve_data_from_elastic():
+        elastic_client = ElasticSearch(es_host=setting.ES_HOST, es_port=setting.ES_PORT,
+                                       es_username=setting.ES_USERNAME,
+                                       es_password=setting.ES_PASSWORD, es_index=setting.ES_INDEX)
+        exclude_ids = Variable.get("exclude_ids", default_var=[], deserialize_json=True)
+        print(type(exclude_ids))
+        elastic_data = elastic_client.receive_data(query={
+            "size": setting.ELASTIC_READ_SIZE,
+            "sort": {
+                setting.ORDERABLE_PARAMETERS: 'desc'
+            },
+            "query": {
+                "bool": {
+                    "must_not": [
+                        {
+                            "exists": {
+                                "field": "lf_subject"
+                            }
+                        },
+                        {
+                            "exists": {
+                                "field": "subject"
+                            }
+                        }
+                    ],
+                    "must": [
+                        {
+                            "exists": {
+                                "field": "ner"
+                            }
+                        },
+                    ]
+                }
+            }
+        })
+        Variable.set("exclude_ids", json.dumps([item['_id'] for item in elastic_data]))
+
+        return elastic_data
+
+
+    @task(max_active_tis_per_dag=6)
+    def perform_subject_v1(batch_tuple):
+        texts = batch_tuple[0]
+        ner_results = batch_tuple[1]
+        topic = topic1()
+        results = []
+        for idx, text in enumerate(texts):
+            try:
+                results.append(topic.infer(text, ner_results[idx]))
+            except Exception as e:
+                print('topic1 Error ========================>> ', e)
+                results.append(None)
+        return results
+
+
+    @task(max_active_tis_per_dag=6)
+    def perform_subject_v2(batch):
+        results = []
+        for idx, text in enumerate(batch):
+            try:
+                results.append(topic2(text))
+            except Exception as e:
+                print('topic2 Error ========================>> ', e)
+                results.append(None)
+        return results
+
+
+    @task()
+    def print_output(raw_data, subject_v1_result, subject_v2_result):
+        kafka_client = Test()
+        flattened_subject_v1_result = [item for sub_list in subject_v1_result for item in sub_list]
+        flattened_subject_v2_result = [item for sub_list in subject_v2_result for item in sub_list]
+        final_data = []
+        for idx, item in enumerate(raw_data):
+            tmp_data = {
+                **item,
+                **flattened_emotion_v1_result[idx],
+                'subject': flattened_subject_v1_result[idx],
+                'lf_subject': flattened_subject_v2_result[idx],
+                'ner': flattened_ner_result[idx],
+                "lf_modules": {
                     "subject": {
                         "version": "1.0.0"
                     }
@@ -244,19 +354,10 @@ with DAG(dag_id="add_emotion_tag_with_expand", default_args=default_args, schedu
 
     raw_data = reterieve_data_from_elastic()
     prepared_data = prepare_data(raw_data)
-    ner_tags = perform_ner.expand(batch=prepared_data)
-    emotion_v1_tags = perform_emotion_v1.expand(texts=prepared_data)
-    sentiment_tags = perform_sentiment.expand(batch=prepared_data)
     subject_v2_tags = perform_subject_v2.expand(batch=prepared_data)
-    prepared_data_for_subject_v1 = prepare_data_for_subject_v1(ner_tags, prepared_data)
     subject_v1_tags = perform_subject_v1.expand(batch_tuple=prepared_data_for_subject_v1)
     final_data = print_output(
         raw_data=raw_data,
-        ner_result=ner_tags,
-        sentimant_result=sentiment_tags,
-        emotion_v1_result=emotion_v1_tags,
         subject_v1_result=subject_v1_tags,
         subject_v2_result=subject_v2_tags,
     )
-
-    # raw_data >> prepared_data >> [ner_tags, emotion_v1_tags, sentiment_tags, subject_v2_tags] >> prepared_data_for_subject_v1 >> subject_v1_tags >> final_data
